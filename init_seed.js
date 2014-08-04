0194a500fb89cbe1c851f4b9ed336f174c53ac6a
sitthidate nanuea <sitthidate@moctis.com>
2014-08-04 10:36:25 +0700

    Update init_seed.js

--- init_seed.js
+++ init_seed.js
@@ -1,9 +1,12 @@
-document.writeln("Hello, World");
-
 getScript = function($src) {
   var imported = document.createElement('script');
   imported.src = $src;
   document.head.appendChild(imported);
 }
+  
+(function() {
+  document.writeln("Hello, World");
+  getScript("loader.js");
+})();
 
-getScript("loader.js");
+
